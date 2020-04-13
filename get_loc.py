cad9442127b6cb042dadd4a58f8753154014c4f1
riskyfrasetio <riskyfrasetio92@gmail.com>
2020-04-13 23:15:01 +0700

    committed20

--- get_loc.py
+++ get_loc.py
@@ -43,8 +43,8 @@ def api_filter():
 	cursor.close()
 	conn.close()
 	#else return Response("not supported".to_json(orient="records"), mimetype='application/json')
-
-port = int(os.environ.get('PORT', 8000))
-app.run(host='127.0.0.1',port=port,debug=True)
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 8000))
+	app.run(host='127.0.0.1',port=port,debug=True)
 
 
